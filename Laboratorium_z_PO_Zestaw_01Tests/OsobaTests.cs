7ce61e9bff2150b18c9efb77e99f2321eb37f84b
MateuszK0808 <mateuszk0808@interia.pl>
2017-11-03 18:22:35 +0100

    Testy Zadanie Domowe

---
+++ Laboratorium_z_PO_Zestaw_01Tests/OsobaTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Laboratorium_z_PO_Zestaw_01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium_z_PO_Zestaw_01.Tests
+{
+    [TestClass()]
+    public class OsobaTests
+    {
+        [TestMethod()]
+        public void OsobaTest()
+        {
+            Osoba osoba = new Osoba();
+            Assert.AreEqual("nieznane", osoba.Imie);
+            Assert.AreEqual("nieznane", osoba.Nazwisko);
+            Assert.AreEqual("nieznany", osoba.AdresZamieszkania);
+        }
+
+        [TestMethod()]
+        public void OsobaTest1()
+        {
+            Osoba osoba = new Osoba("Jan", "Kowalski", "adres");
+            Assert.AreEqual("Jan", osoba.Imie);
+            Assert.AreEqual("Kowalski", osoba.Nazwisko);
+            Assert.AreEqual("adres", osoba.AdresZamieszkania);
+        }
+    }
+}
