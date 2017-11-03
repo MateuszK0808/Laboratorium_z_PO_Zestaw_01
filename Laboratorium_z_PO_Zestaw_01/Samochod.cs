8dde6c52965b03e799774b4fd594aa069751b38e
MateuszK0808 <mateuszk0808@interia.pl>
2017-11-03 13:46:45 +0100

    Zadanie 1

---
+++ Laboratorium_z_PO_Zestaw_01/Samochod.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium_z_PO_Zestaw_01
+{
+    public class Samochod
+    {
+        private string marka;
+        private string model;
+        private int iloscDrzwi;
+        private int pojemnoscSilnika;
+        private double srednieSpalanie;
+        private static int liczbaSamochodow = 0;
+
+        public Samochod()
+        {
+            liczbaSamochodow++;
+            Marka = "nieznana";
+            Model = "nieznany";
+            IloscDrzwi = 0;
+            PojemnoscSilnika = 0;
+            SrednieSpalanie = 0.0;
+        }
+
+        public Samochod(
+            string marka_,
+            string model_,
+            int iloscDrzwi_,
+            int pojemnoscSilnika_,
+            double srednieSpalanie_
+            )
+        {
+            liczbaSamochodow++;
+            Marka = marka_;
+            Model = model_;
+            IloscDrzwi = iloscDrzwi_;
+            PojemnoscSilnika = pojemnoscSilnika_;
+            SrednieSpalanie = srednieSpalanie_;
+        }
+
+        public string Marka
+        {
+            get { return marka; }
+            set { marka = value;  }
+        }
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+        public int IloscDrzwi
+        {
+            get { return iloscDrzwi; }
+            set { iloscDrzwi = value; }
+        }
+        public int PojemnoscSilnika
+        {
+            get { return pojemnoscSilnika; }
+            set { pojemnoscSilnika = value; }
+        }
+        public double SrednieSpalanie
+        {
+            get { return srednieSpalanie; }
+            set { srednieSpalanie = value; }
+        }
+
+        private double ObliczSpalanie(double dlugoscTrasy)
+        {
+            double spalanie = (srednieSpalanie * dlugoscTrasy) / 100.0;
+            return spalanie;
+        }
+
+        public double ObliczKosztPrzejazdu(double dlugoscTrasy, double cenaPaliwa)
+        {
+            double spalanie = ObliczSpalanie(dlugoscTrasy);
+            double kosztPrzejazdu = spalanie * cenaPaliwa;
+            return kosztPrzejazdu;
+        }
+
+        public void WypiszInfo()
+        {
+            Console.WriteLine("Marka: {0}", Marka);
+            Console.WriteLine("Model: {0}", Model);
+            Console.WriteLine("IlośćDrzwi: {0}", IloscDrzwi);
+            Console.WriteLine("Pojemność silnika: {0}", PojemnoscSilnika);
+            Console.WriteLine("Średnie spalanie: {0}", SrednieSpalanie);
+        }
+
+        public static void WypiszIloscSamochodow()
+        {
+            Console.WriteLine("Ilość samochodów: {0}", liczbaSamochodow);
+        }
+    }
+}
---
+++ Laboratorium_z_PO_Zestaw_01Tests/SamochodTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Laboratorium_z_PO_Zestaw_01;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorium_z_PO_Zestaw_01.Tests
+{
+    [TestClass()]
+    public class SamochodTests
+    {
+        [TestMethod()]
+        public void SamochodTest()
+        {
+            var samochod = new Samochod();
+            Assert.AreEqual("nieznana", samochod.Marka);
+            Assert.AreEqual("nieznany", samochod.Model);
+            Assert.AreEqual(0, samochod.IloscDrzwi);
+            Assert.AreEqual(0, samochod.PojemnoscSilnika);
+            Assert.AreEqual(0.0, samochod.SrednieSpalanie);
+        }
+
+        [TestMethod()]
+        public void SamochodTest1()
+        {
+
+            var s2 = new Samochod("Syrena", "105", 2, 800, 7.6);
+
+            Assert.AreEqual("Syrena", s2.Marka);
+            Assert.AreEqual("105", s2.Model);
+            Assert.AreEqual(2, s2.IloscDrzwi);
+            Assert.AreEqual(800, s2.PojemnoscSilnika);
+            Assert.AreEqual(7.6, s2.SrednieSpalanie);
+        }
+    }
+}
